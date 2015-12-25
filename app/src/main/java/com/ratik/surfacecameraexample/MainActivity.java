1c7582390e1153fc7643111efd1f46ee73dfdf6b
Ratik Sharma <Ratik96@gmail.com>
2015-12-25 16:25:02 +0530

    Refactor

--- app/src/main/java/com/ratik/surfacecameraexample/MainActivity.java
+++ app/src/main/java/com/ratik/surfacecameraexample/MainActivity.java
@@ -1,45 +1,16 @@
 package com.ratik.surfacecameraexample;
 
-import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.FrameLayout;
-import android.widget.ImageButton;
 
-@SuppressWarnings("deprecation")
 public class MainActivity extends AppCompatActivity {
-
-    private ImageButton shutterButton;
-    private ImageButton rotateButton;
-
-    private Camera camera;
-    private CameraPreview cameraPreview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        shutterButton = (ImageButton) findViewById(R.id.shutter);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        // Get an instance of the Camera
-        camera = CameraHelper.getCameraInstance();
-
-        // Create our Preview view and set it as the content of our activity.
-        cameraPreview = new CameraPreview(this, camera);
-        FrameLayout preview = (FrameLayout) findViewById(R.id.cameraPreview);
-        preview.addView(cameraPreview);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (camera != null) {
-            camera.release();
-        }
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.mainContent, new CameraFragment()).commit();
     }
 }
